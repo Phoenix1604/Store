0d45d8b7092e1361e500814c4cda34a1832e03ae
Subhajit Bera <subhajit.bera@wisdmlabs.com>
2023-04-04 11:40:07 +0530

    Boiler plate code added

---
+++ includes/class-store-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wisdmlabs.com
+ * @since      1.0.0
+ *
+ * @package    Store
+ * @subpackage Store/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Store
+ * @subpackage Store/includes
+ * @author     Subhajit Bera <subhajit.bera@wisdmlabs.com>
+ */
+class Store_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-store-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wisdmlabs.com
+ * @since      1.0.0
+ *
+ * @package    Store
+ * @subpackage Store/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Store
+ * @subpackage Store/includes
+ * @author     Subhajit Bera <subhajit.bera@wisdmlabs.com>
+ */
+class Store_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
